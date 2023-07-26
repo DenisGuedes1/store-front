9f20b91f9e9cd71234c4110afd3528356f48822f
LucasDantas77 <lucasdantasrodrigues847@gmail.com>
2023-07-26 13:57:38 -0300

    feat: adicionando dashboard

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,12 @@
 import { Route, Routes } from "react-router-dom";
-import { FormRegister } from "../components/FormRegister";
 import { Login } from "../pages/Login";
+import { Register } from "../pages/Register";
 
 export const RoutesMain = () => {
   return (
     <Routes>
       <Route path="/" />
-      <Route path="register" element={<FormRegister />} />
+      <Route path="register" element={<Register />} />
       <Route path="login" element={<Login />} />
       <Route path="admin" />
     </Routes>
