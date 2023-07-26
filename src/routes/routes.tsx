c26bf3de7db4bd9f110b5941fce568018d16fd38
LucasDantas77 <lucasdantasrodrigues847@gmail.com>
2023-07-26 13:54:09 -0300

    feat: atualizando routes com pagina login

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,13 +1,13 @@
 import { Route, Routes } from "react-router-dom";
-import { FormLogin } from "../components/FormLogin";
 import { FormRegister } from "../components/FormRegister";
+import { Login } from "../pages/Login";
 
 export const RoutesMain = () => {
   return (
     <Routes>
       <Route path="/" />
       <Route path="register" element={<FormRegister />} />
-      <Route path="login" element={<FormLogin />} />
+      <Route path="login" element={<Login />} />
       <Route path="admin" />
     </Routes>
   );
