3992064173226d169e193ed42d685ed7481aef49
LucasDantas77 <lucasdantasrodrigues847@gmail.com>
2023-07-26 14:06:15 -0300

    feat: adicionando admin page na routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from "react-router-dom";
 import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
 import { Dashboard } from "../pages/Dashboard";
+import { AdminPage } from "../pages/AdminPage";
 
 export const RoutesMain = () => {
   return (
@@ -9,7 +10,7 @@ export const RoutesMain = () => {
       <Route path="/" element={<Dashboard />} />
       <Route path="register" element={<Register />} />
       <Route path="login" element={<Login />} />
-      <Route path="admin" />
+      <Route path="admin" element={<AdminPage/>} />
     </Routes>
   );
 };
