cb0e0e3dad51bbec22c3b2c149df59d828c6971d
LucasDantas77 <lucasdantasrodrigues847@gmail.com>
2023-07-26 13:59:03 -0300

    feat: adicionando dashboard na routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,11 +1,12 @@
 import { Route, Routes } from "react-router-dom";
 import { Login } from "../pages/Login";
 import { Register } from "../pages/Register";
+import { Dashboard } from "../pages/Dashboard";
 
 export const RoutesMain = () => {
   return (
     <Routes>
-      <Route path="/" />
+      <Route path="/" element={<Dashboard />} />
       <Route path="register" element={<Register />} />
       <Route path="login" element={<Login />} />
       <Route path="admin" />
