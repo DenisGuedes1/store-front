1f23224790c7d7dccb6dafc60b5576b14277216c
LucasDantas77 <lucasdantasrodrigues847@gmail.com>
2023-07-26 13:50:36 -0300

    feat:adicionando componente register na routes

--- src/routes/routes.tsx
+++ src/routes/routes.tsx
@@ -1,12 +1,13 @@
 import { Route, Routes } from "react-router-dom";
 import { FormLogin } from "../components/FormLogin";
+import { FormRegister } from "../components/FormRegister";
 
 export const RoutesMain = () => {
   return (
     <Routes>
-      <Route path="/" element={<FormLogin />} />
-      <Route path="register" />
-      <Route path="login" />
+      <Route path="/" />
+      <Route path="register" element={<FormRegister />} />
+      <Route path="login" element={<FormLogin />} />
       <Route path="admin" />
     </Routes>
   );
